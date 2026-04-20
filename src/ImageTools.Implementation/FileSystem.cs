bca33e433319e2ba03e3acf24af8d6f505625747
Artem Agafonov <agart@mail.ru>
2026-04-20 22:22:07 +0300

    Add IFileSystem with implementation.

---
+++ src/ImageTools.Implementation/FileSystem.cs
@@ -0,0 +1,89 @@
+﻿using ImageTools.Interfaces;
+
+namespace ImageTools.Implementation;
+
+/// <summary>
+/// File system operations.
+/// </summary>
+public class FileSystem : IFileSystem
+{
+    /// <inheritdoc/>
+    public bool FileExists(string path) =>
+        File.Exists(path);
+
+    /// <inheritdoc/>
+    public string GetCurrentDirectory()
+    {
+        try
+        {
+            return AppContext.BaseDirectory;
+        }
+        catch (ImageToolsException)
+        {
+            throw;
+        }
+        catch (Exception ex)
+        {
+            var code = ImageToolsExceptionCode.GetCurrentDirectoryError;
+            throw new ImageToolsException(code, ex.Message);
+        }
+    }
+
+    /// <inheritdoc/>
+    public ICollection<string> GetFiles(string searchPattern)
+    {
+        try
+        {
+            var path = GetCurrentDirectory();
+            var option = SearchOption.TopDirectoryOnly;
+            return Directory.GetFiles(path, searchPattern, option);
+        }
+        catch (ImageToolsException)
+        {
+            throw;
+        }
+        catch (Exception ex)
+        {
+            var code = ImageToolsExceptionCode.GetFilesFromDirectoryError;
+            throw new ImageToolsException(code, ex.Message);
+        }
+    }
+
+    /// <inheritdoc/>
+    public ICollection<string> GetFilesRecursive(string searchPattern)
+    {
+        try
+        {
+            var path = GetCurrentDirectory();
+            var option = SearchOption.AllDirectories;
+            return Directory.GetFiles(path, searchPattern, option);
+        }
+        catch (ImageToolsException)
+        {
+            throw;
+        }
+        catch (Exception ex)
+        {
+            var code = ImageToolsExceptionCode.GetFilesFromDirectoryRecursive;
+            throw new ImageToolsException(code, ex.Message);
+        }
+    }
+
+    /// <inheritdoc/>
+    public void CopyFile(string sourcePath, string destinationPath)
+    {
+        try
+        {
+            File.Copy(sourcePath, destinationPath, overwrite: true);
+        }
+        catch (ImageToolsException)
+        {
+            throw;
+        }
+        catch (Exception ex)
+        {
+            var code = ImageToolsExceptionCode.CopyFileError;
+            throw new ImageToolsException(code, ex.Message);
+        }
+    }
+}
---
+++ src/ImageTools.Interfaces/IFileSystem.cs
@@ -0,0 +1,45 @@
+﻿namespace ImageTools.Interfaces;
+
+/// <summary>
+/// Common file system operations.
+/// </summary>
+public interface IFileSystem
+{
+    /// <summary>
+    /// Checks if the file exists at the specified path.
+    /// </summary>
+    /// <param name="path">the path to the file.</param>
+    /// <returns>True if the file exists; otherwise, false.</returns>
+    bool FileExists(string path);
+
+    /// <summary>
+    /// Gets the absolute path to the current application directory.
+    /// </summary>
+    /// <returns>The current directory path.</returns>
+    /// <exception cref="ImageToolsException">Thrown when directory access fails.</exception>
+    string GetCurrentDirectory();
+
+    /// <summary>
+    /// Retrieves files from the current directory matching the pattern.
+    /// </summary>
+    /// <param name="searchPattern">The search string (e.g., "*.jpg").</param>
+    /// <returns>A collection of file paths.</returns>
+    /// <exception cref="ImageToolsException">Thrown if search fails.</exception>
+    ICollection<string> GetFiles(string searchPattern);
+
+    /// <summary>
+    /// Retrieves files from the current directory and subdirectories.
+    /// </summary>
+    /// <param name="searchPattern">The search string (e.g., "*.png").</param>
+    /// <returns>A collection of file paths.</returns>
+    /// <exception cref="ImageToolsException">Thrown if recursive search fails.</exception>
+    ICollection<string> GetFilesRecursive(string searchPattern);
+
+    /// <summary>
+    /// Copies an existing file to a new location.
+    /// </summary>
+    /// <param name="sourcePath">The path of the file to copy.</param>
+    /// <param name="destinationPath">The path to the new file.</param>
+    /// <exception cref="ImageToolsException">Thrown if copy process fails.</exception>
+    void CopyFile(string sourcePath, string destinationPath);
+}
