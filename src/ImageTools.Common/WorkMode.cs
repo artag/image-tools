9d8dfe97a8a49283df67746ec6eb41d9d08f6c7e
Artem Agafonov <agart@mail.ru>
2026-04-21 22:03:03 +0300

    Common. Add WorkMode.

---
+++ src/ImageTools.Common/WorkMode.cs
@@ -0,0 +1,22 @@
+﻿namespace ImageTools.Common;
+
+/// <summary>
+/// Application work mode.
+/// </summary>
+public static class WorkMode
+{
+    /// <summary>
+    /// Process single file.
+    /// </summary>
+    public const string ProcessFile = "ProcessFile";
+
+    /// <summary>
+    /// Process files in current (single) directory.
+    /// </summary>
+    public const string ProcessFilesInDirectory = "ProcessFilesInDirectory";
+
+    /// <summary>
+    /// Process files in currenct directory and subdirectories.
+    /// </summary>
+    public const string ProcessFilesInDirectortyRecursive = "ProcessFilesInDirectortyRecursive";
+}
