7ef322053c24509171c65e7309782add3c4e2193
Artem Agafonov <agart@mail.ru>
2026-04-21 22:03:52 +0300

    Common. Update BaseProgram.

--- src/ImageTools.Common/BaseProgram.cs
+++ src/ImageTools.Common/BaseProgram.cs
@@ -1,20 +1,19 @@
-﻿using ImageTools.Common;
-using ImageTools.Interfaces;
+﻿using ImageTools.Interfaces;
 
-namespace ImageTools.Implementation;
+namespace ImageTools.Common;
 
 /// <summary>
 /// Base program.
 /// </summary>
-/// <typeparam name="T">Options name.</typeparam>
 /// <param name="appName">Application name.</param>
 /// <param name="versionProvider">Application version provider.</param>
+/// <param name="fileSystem">Common file system operations.</param>
 /// <param name="log">Logger.</param>
-public abstract class BaseProgram<T>(
+public abstract class BaseProgram(
     string appName,
     IVersionProvider versionProvider,
+    IFileSystem fileSystem,
     ILog log)
-    where T : Enum
 {
     /// <summary>
     /// Run program.
@@ -22,15 +21,17 @@ public abstract class BaseProgram<T>(
     /// <param name="args">Input arguments from CLI.</param>
     /// <param name="options">Command line options.</param>
     /// <returns>Asynchronous operation.</returns>
-    public async Task Run(ICollection<string> args, ICollection<Option<T>> options)
+#pragma warning disable SP2101 // Method is too long
+    public async Task Run(ICollection<string> args, ICollection<Option> options)
+#pragma warning restore SP2101 // Method is too long
     {
         var usage = new Usage(appName, versionProvider, log);
-        var cli = new Arguments<T>();
+        var defaultOptions = CreateDefaultOptions();
+        var allOptions = defaultOptions.Union(options).ToArray();
 
         try
         {
-            var opts = cli.Process(args);
-            await Process(opts);
+            await RunInternal(args, allOptions);
         }
         catch (OperationCanceledException)
         {
@@ -39,7 +40,8 @@ public abstract class BaseProgram<T>(
         catch (ImageToolsException ex)
         {
             log.LogError(ex.Message);
-            usage.ShowUsage(options);
+            if (ex.Code is not ImageToolsExceptionCode.ErrorOnImageProcessing)
+                usage.ShowUsage(allOptions);
         }
         catch (Exception ex)
         {
@@ -50,7 +52,76 @@ public abstract class BaseProgram<T>(
     /// <summary>
     /// Process image(s) using options.
     /// </summary>
+    /// <param name="path">Filename to process.</param>
     /// <param name="options">Options.</param>
     /// <returns>Asynchronous operation.</returns>
-    protected abstract Task Process(ICollection<Option<T>> options);
+    protected abstract Task Process(string path, Options options);
+
+    private static Arguments ConfigureArgumentsHandler(Option[] options)
+    {
+        var cli = new Arguments(options);
+        cli.AddExclusiveGroup("-f", "-d", "-a");
+        return cli;
+    }
+
+    private static Option[] CreateDefaultOptions()
+    {
+        const string groupName = "Application work mode options";
+        return [
+            new StringOption(WorkMode.ProcessFile)
+            {
+                Names = ["-f", "--file"],
+                Description = "Process single file",
+                GroupName = groupName,
+            },
+            new BooleanOption(WorkMode.ProcessFilesInDirectory)
+            {
+                Names = ["-d", "--dir"],
+                Description = "Process files in current (single) directory",
+                GroupName = groupName,
+            },
+            new BooleanOption(WorkMode.ProcessFilesInDirectortyRecursive)
+            {
+                Names = ["-a", "--alldir"],
+                Description = "Process files in current directory and subdirectories",
+                GroupName = groupName,
+            },
+        ];
+    }
+
+    private static Option GetWorkModeOption(Options options) =>
+        options.GetOption(
+            WorkMode.ProcessFile,
+            WorkMode.ProcessFilesInDirectory,
+            WorkMode.ProcessFilesInDirectortyRecursive);
+
+    private async Task RunInternal(ICollection<string> args, Option[] opts)
+    {
+        var cli = ConfigureArgumentsHandler(opts);
+        var options = cli.Process(args);
+        var mode = GetWorkModeOption(options);
+        if (mode.OptionName is WorkMode.ProcessFile)
+        {
+            var filename = mode.Value as string;
+            await Process(filename!, options);
+            return;
+        }
+        else if (mode.OptionName is WorkMode.ProcessFilesInDirectory)
+        {
+            var files = fileSystem.GetFiles(".jpg");
+            await ProcessMultipleFiles(files, options);
+            return;
+        }
+        else
+        {
+            var files = fileSystem.GetFilesRecursive(".jpg");
+            await ProcessMultipleFiles(files, options);
+        }
+    }
+
+    private async Task ProcessMultipleFiles(ICollection<string> files, Options options)
+    {
+        foreach (var file in files)
+            await Process(file, options);
+    }
 }
