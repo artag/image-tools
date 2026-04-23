3f368b0d589d64a5e2b674522ff2443792811cec
Artem Agafonov <agart@mail.ru>
2026-04-23 22:15:47 +0300

    Fix build.cmd.

--- src/build.cmd
+++ src/build.cmd
@@ -1,1 +1,1 @@
-dotnet publish GfyRC.Service/GfyRC.Service.csproj -r win-x64 -c Release -o ../pub/win-x64 --self-contained true -p:PublishTrimmed=true
+dotnet publish ImageTools.Square/ImageTools.Square.csproj -r win-x64 -c Release -o ../pub/win-x64 --self-contained true -p:PublishTrimmed=true
