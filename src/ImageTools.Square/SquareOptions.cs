b2db14f5fe5667537f2f83e6e33c4607aebd258b
Artem Agafonov <agart@mail.ru>
2026-04-23 22:14:24 +0300

    Square. Add SquareOptions.

---
+++ src/ImageTools.Square/SquareOptions.cs
@@ -0,0 +1,32 @@
+﻿namespace ImageTools.Square;
+
+/// <summary>
+/// Square option types.
+/// </summary>
+internal enum SquareOptions
+{
+    /// <summary>
+    /// Make backup.
+    /// </summary>
+    MakeBackup,
+
+    /// <summary>
+    /// Blurring degree.
+    /// </summary>
+    Blurring,
+
+    /// <summary>
+    /// Shading degree.
+    /// </summary>
+    Shading,
+
+    /// <summary>
+    /// Image quality.
+    /// </summary>
+    Quality,
+
+    /// <summary>
+    /// Threshold to process. The minimal difference between image width and height.
+    /// </summary>
+    Threshold,
+}
