c06a90018b06066de443b0f3ee17d1cbb40cded5
Artem Agafonov <agart@mail.ru>
2026-04-23 22:12:44 +0300

    Square. Add AssemblyInfo with InternalsVisibleTo attribute (for unit tests).

---
+++ src/ImageTools.Square/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+#pragma warning disable SP2100 // Code line is too long
+
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: Guid("729dce8a-45d4-493a-ab5b-6c1809749c46")]
+[assembly: InternalsVisibleTo("ImageTools.Unit.Tests, PublicKey=00240000048000009400000006020000002400005253413100040000010001002d6fe79e532d545adc3b9291dd46279fa2c389799ac2a70b533dc879954ff87e839a81c65c3842a86e7eabadcf43262ce3ba16d026949484f322c21706ca12279382912fb7e01ede6cff4e8e628dd9fdde730cd5aba849b49785286edd0d05209cf8a6ff12facbc40c8fb8f2ef9dd1c0aa6df8827910ef7abd1ecaaf7b3ebea9")]
+
+#pragma warning restore SP2100 // Code line is too long
